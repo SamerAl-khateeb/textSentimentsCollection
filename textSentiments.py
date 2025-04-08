5c8d8833fe6bd45b9fb38f920a9baff0a114fa3f
Samer Al-khateeb <sxalkhateeb@ualr.edu>
2025-04-08 12:59:09 -0500

    updated code Spring 2025

--- textSentiments.py
+++ textSentiments.py
@@ -1,11 +1,11 @@
 # textSentiments.py                             By: Samer Al-khateeb
-# a script that use NLTK library and a pretrained sentiment analyzer 
+# A script that uses the NLTK library and a pretrained sentiment analyzer 
 # called VADER (Valence Aware Dictionary and sEntiment Reasoner).
-# It is good for short social media posts, however, it does not 
+# It is good for short social media posts; however, it does not 
 # perform well for long text. This code was built based on this page:
 # https://realpython.com/python-nltk-sentiment-analysis/
 
-# Dependencies: you will need to install the following library 
+# Dependencies: You will need to install the following library 
 # before you can run this code.
 # For Mac users, open terminal and type:
 #       python3 -m pip install nltk
@@ -13,12 +13,11 @@
 #       py -m pip install nltk
 
 
-
 import nltk
-# the line below needs to run only once
+# The line below needs to run only once
 nltk.download('vader_lexicon')
 
-# if you receive an error stating you can not download vader_lexicon
+# If you receive an error stating you cannot download vader_lexicon,
 # comment the nltk.download('vader_lexicon') line and uncomment the 
 # block of code below
 
@@ -39,51 +38,40 @@ import csv
 
 
 def write_output_to_CSV(biglist):
-        columnNames = ["ID", "Text", "Sentiment Score"]
-        filename = "sentimentScoresOutput.csv"
-        # creating a file to save the output
-        with open(filename, 'w', newline='', encoding='utf-8') as csvOutputFile:
-                # creating a csv writer object
-                csvwriter = csv.writer(csvOutputFile, delimiter=',', lineterminator='\n')
-                #write the columns headers
-                csvwriter.writerow(columnNames)
-                csvwriter.writerows(biglist)
-
+    columnNames = ["ID", "Text", "Sentiment Score"]
+    filename = "sentimentScoresOutput.csv"
+    # Creating a file to save the output
+    with open(filename, 'w', newline='', encoding='utf-8') as csvOutputFile:
+        csvwriter = csv.writer(csvOutputFile, delimiter=',', lineterminator='\n')
+        csvwriter.writerow(columnNames)  # Write column headers
+        csvwriter.writerows(biglist)     # Write the data rows
 
 def main():
-	# set the file name here
-	CSVInputFileName = 'input.csv'
+    # Set the file name here
+    CSVInputFileName = 'input.csv'
 
-	# creating an instance of the class
-	sia = SentimentIntensityAnalyzer()
-	
-	# creating a list to hold the output
-	CSVoutputList = []
+    # Creating an instance of the SentimentIntensityAnalyzer
+    sia = SentimentIntensityAnalyzer() 
 
-	# open the input file and read it
-	with open(CSVInputFileName, newline='', encoding='utf-8') as csvInputFile:
-		CSVFileAsList = csv.reader(csvInputFile, skipinitialspace=True)
-		# skipping the first row in the csv input file
-		next(CSVFileAsList)
-		# for each row in the CSV file
-		for row in CSVFileAsList:
-			text = row[1]
-			# this give you all the sentiments positive, 
-			# negative, neutral (add up to 1), and compound
-			#sentiments = sia.polarity_scores(text)
+    # Creating a list to hold the output
+    CSVoutputList = []
 
-			# this give you a score between [-1,1]
-			sentiments = sia.polarity_scores(text)["compound"]
-			# create a csv row
-			CSVRow = [row[0], row[1], sentiments]
-			# append the row to the output list
-			CSVoutputList.append(CSVRow)
-			
-			print(CSVRow)
-			print()
-		# write the list to a CSV file
-		write_output_to_CSV(CSVoutputList)
+    # Open the input file and read it
+    with open(CSVInputFileName, newline='', encoding='utf-8') as csvInputFile:
+        CSVFileAsList = csv.reader(csvInputFile, skipinitialspace=True)
+        next(CSVFileAsList)  # Skip header row
 
+        # Process each row in the CSV file
+        for row in CSVFileAsList:
+            text = row[1]
+            sentiments = sia.polarity_scores(text)["compound"]
+            CSVRow = [row[0], row[1], sentiments]
+            CSVoutputList.append(CSVRow)
+            print(CSVRow)
+            print()
+
+    # Write the list to a CSV file
+    write_output_to_CSV(CSVoutputList)
 
 if __name__ == "__main__":
     main()
